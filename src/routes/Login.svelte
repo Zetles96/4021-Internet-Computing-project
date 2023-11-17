ecc5af2d277e5a98a4b84f68324759f5668f0784
Norton Choy <nomvus848@gmail.com>
2023-11-17 17:34:25 +0800

    added register button

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -9,11 +9,19 @@
 		});
 	}
 
+	function doRegister() {
+		dispatch('register', {
+			success: true
+		});
+	}
+
 </script>
 
+
+<!--	Design Login and Sign up modal here -->
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
 	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
-	  <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+	  <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log in to your account</h2>
 	</div>
   
 	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -21,7 +29,7 @@
 		<div>
 		  <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
 		  <div class="mt-2">
-			<input id="sigin-username" name="signin-username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+			<input id="login-username" name="login-username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
 		  </div>
 		</div>
   
@@ -34,9 +42,10 @@
 		  </div>
 		</div>
   
-		<div>
-		  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-		</div>
+		<div class="flex flex-col">
+			<button type="submit" on:click={doLogin} class="w-full mb-2 rounded-md bg-indigo-600 px-4 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring focus:border-indigo-300">Log in</button>
+			<button type="submit" on:click={doRegister} class="w-full rounded-md bg-indigo-600 px-4 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring focus:border-indigo-300">Register</button>
+		</div>			
 	  </form>
 	</div>
   </div>
