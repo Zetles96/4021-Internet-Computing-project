a4bd45fa07a2483c1f6fc99b03860e452ccf5950
HeroGamers <hero@herogamers.dev>
2023-11-21 15:13:37 +0800

    Add grass move

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -19,6 +19,24 @@
 
 	const player_pos = [0, 0];
 
+	// Make canvasimagesource from grass tile image string
+	const grassTile = new Image();
+	grassTile.src = GrassTile;
+
+	// Player image
+	const player = new Image();
+	player.src = Player;
+
+	/**
+	 * https://stackoverflow.com/a/17323608/12418245
+	 * @param {number} n
+	 * @param {number} m
+	 * @returns {number}
+	 */
+	function mod(n, m) {
+		return ((n % m) + m) % m;
+	}
+
 	function toMenu() {
 		dispatch('back');
 	}
@@ -35,26 +53,26 @@
 	 * @param {KeyboardEvent} e
 	 */
 	const handleKeydown = (e) => {
+		console.log("Key pressed: ", e.key);
 		if (e.key === 'Escape') {
 			toMenu();
 		}
-		// TODO: this crashes the game, for some reason, gotta fix
-		// if (e.key === 'ArrowUp') {
-		// 	player_pos[1] -= 16;
-		// 	draw();
-		// }
-		// if (e.key === 'ArrowDown') {
-		// 	player_pos[1] += 16;
-		// 	draw();
-		// }
-		// if (e.key === 'ArrowLeft') {
-		// 	player_pos[0] -= 16;
-		// 	draw();
-		// }
-		// if (e.key === 'ArrowRight') {
-		// 	player_pos[0] += 16;
-		// 	draw();
-		// }
+		if (e.key === 'ArrowUp' || e.key.toLowerCase() === 'w') {
+			player_pos[1] -= 16;
+			draw();
+		}
+		if (e.key === 'ArrowDown' || e.key.toLowerCase() === 's') {
+			player_pos[1] += 16;
+			draw();
+		}
+		if (e.key === 'ArrowLeft' || e.key.toLowerCase() === 'a') {
+			player_pos[0] -= 16;
+			draw();
+		}
+		if (e.key === 'ArrowRight' || e.key.toLowerCase() === 'd') {
+			player_pos[0] += 16;
+			draw();
+		}
 	};
 
 
@@ -77,89 +95,85 @@
 
 	$ : if(ctx) {
 		ctx.imageSmoothingEnabled = false;
-		draw();
 
-		// Add event listener for keydown
-		window.addEventListener('keydown', handleKeydown);
+		// Only use the images after they are loaded
+		grassTile.onload = () => {
+			draw();
+
+			// Add event listener for keydown
+			window.addEventListener('keydown', handleKeydown);
+		};
 	}
 
 	function drawCharacter() {
 		if (ctx) {
-			const player = new Image();
-			player.src = Player;
-
-			player.onload = () => {
-				if (!ctx) return;
+			if (player.complete) {
 				ctx.drawImage(player, 0, 0, 128, 128, canvas.width/2 - 128 / 2, canvas.height/2 - 128 / 2, 128, 128);
+			}
+			else {
+				player.onload = () => {
+					drawCharacter();
+				}
 			}
 		}
 	}
 
 
 	function draw() {
-		if(ctx) {
+		if(ctx && grassTile.complete) {
+			// console.clear();
+
 			console.log("Drawing with player position: ", player_pos);
 
-			// Make canvasimagesource from grass tile image string
-			const grassTile = new Image();
-			grassTile.src = GrassTile;
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-			// Only use the image after it's loaded
-			grassTile.onload = () => {
-				if (!ctx) return;
-				// https://stackoverflow.com/a/17247032/12418245
-				let w = 32;
-				let h = 32;
+			// https://stackoverflow.com/a/17247032/12418245
+			let w = 32;
+			let h = 32;
 
-				let x = player_pos[0] % canvas.width;
-				let y = player_pos[1] % canvas.height;
+			const x_mod = mod(player_pos[0], canvas.width);
+			const y_mod = mod(player_pos[1], canvas.height);
+			let x = x_mod+w;
+			let y = y_mod+h;
 
-				// the following line needs to exist, otherwise the canvas will be blank until
-				// player position, for some reason..?
-				ctx.drawImage(grassTile, 0, 0, w, h);
-				// console.clear();
-				// console.log(x, y);
-				ctx.drawImage(grassTile, x, y, w, h);
-				x += w;
-				x = x % canvas.width;
-				y += h;
-				y = y % canvas.height;
 
-				while (x !== (player_pos[0] % canvas.width)) {
-					// console.log(x, y);
-					while (y !== (player_pos[1] % canvas.height)) {
-						ctx.drawImage(grassTile, x, y, w, h);
-						y += h;
-						y = y % canvas.height;
+			while (true) {
+				y = y_mod+h;
+				while (true) {
+					// console.log("Drawing grass tile at: ", [x, y]);
+					ctx.drawImage(grassTile, x, y, w, h);
+
+					if (y === y_mod) {
+						// console.log("y === player_pos[1] % canvas.height", y, y_mod);
+						break;
 					}
+					else if (y+h > canvas.height) {
+						// We have to calculate the new y to draw from beginning of canvas (or before)
+						// and add height enough times to get to player_pos[1] % canvas.height
+						y = y_mod - h * Math.ceil(y_mod / h);
+					}
+					else {
+						y += h;
+						y = mod(y, canvas.height);
+					}
+				}
+
+				if (x === x_mod) {
+					break;
+				}
+				else if (x+w > canvas.width) {
+					// We have to calculate the new x to draw from beginning of canvas (or before)
+					// and add width enough times to get to player_pos[0] % canvas.width
+					x = x_mod - w * Math.ceil(x_mod / w);
+				}
+				else {
 					x += w;
-					x = x % canvas.width;
+					x = mod(x, canvas.width);
 				}
+			}
 
-				//draw once
-				ctx.drawImage(grassTile, player_pos[0], player_pos[1], w, h);
-
-				while (w < canvas.width) {
-					ctx.drawImage(canvas, w, 0);
-					w <<= 1;  // shift left 1 = *2 but slightly faster
-				}
-				while (h < canvas.height) {
-					ctx.drawImage(canvas, 0, h);
-					h <<= 1;
-				}
-
-				// // Fill background with grasstile
-				// const pattern = context.createPattern(grassTile, "repeat");
-				// if (!pattern) {
-				// 	console.log('pattern is null');
-				// 	return;
-				// }
-				// context.fillStyle = pattern;
-				// context.fillRect(0, 0, canvas.width, canvas.height);
-
-				// Draw the character
-				drawCharacter();
-			};
+			// Draw the character
+			drawCharacter();
 		}
 	}
 </script>
